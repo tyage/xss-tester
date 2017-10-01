c95a3a07a42aba0c1fead8bd085e8d0bd4247c50
tyage <namatyage@gmail.com>
2017-10-01 23:39:32 +0900

    detect alert test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 import assert from 'assert';
+import xssTester from '../src/index.js';
 
 describe('simple xss tester', () => {
-  it ('detect attack if alert is up', () => {
+  it ('detect attack if alert is up', async () => {
+    const url = 'http://localhost:3000/vuln?q=<script>alert(1)</script>';
+    const result = await xssTester.test(url);
+    assert.equal(result, false);
   });
 });
