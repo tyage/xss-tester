272a14b23a454cda6d906696c9296148486f882b
tyage <namatyage@gmail.com>
2017-10-02 15:51:43 +0900

    stored xss and reflected xss test

--- test/index.js
+++ test/index.js
@@ -2,18 +2,28 @@ import assert from 'assert';
 import express from 'express';
 import xssTester from '../src/index.js';
 
-describe('simple xss tester', () => {
+describe('simple xss tester', function() {
+  this.timeout(5000);
+
   before(() => {
     const app = express();
-    app.get('/vuln', (req, res) => {
+    app.get('/reflected', (req, res) => {
       console.log(req.query);
       res.send(req.query.q);
+    });
+    app.get('/stored', (req, res) => {
+      res.send('<script>alert(1)</script>');
     });
     app.listen(3000);
   });
 
-  it ('detect attack if alert is up', async () => {
-    const url = 'http://localhost:3000/vuln?q=<script>alert(1)</script>';
+  it ('detect stored xss when alert up', async () => {
+    const url = 'http://localhost:3000/stored';
+    const result = await xssTester.test(url);
+    assert.equal(result, false);
+  });
+  it ('detect reflected xss when alert up', async () => {
+    const url = 'http://localhost:3000/reflected?q=<script>alert(1)</script>';
     const result = await xssTester.test(url);
     assert.equal(result, false);
   });
